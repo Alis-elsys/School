a4ead752bb4acca470e72395861793858f947c44
Alis <71932909+Alis-elsys@users.noreply.github.com>
2023-06-10 15:12:12 +0300

    Smart SOS

---
+++ VMKS/nodeMCU/nodeMCU.ino
@@ -0,0 +1,65 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+#include <UrlEncode.h>
+
+const char* ssid = "AndroidAP";
+const char* password = "...";
+
+String phoneNumber = "+359876276918";
+String apiKey = "9594908";
+int var = 0;
+int pushPin = 14;
+
+void sendMessage(String message){
+
+  // Data to send with HTTP POST
+  String url = "http://api.callmebot.com/whatsapp.php?phone=" + phoneNumber + "&apikey=" + apiKey + "&text=" + urlEncode(message);
+  WiFiClient client;    
+  HTTPClient http;
+  http.begin(client, url);
+
+  // Specify content-type header
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  
+  // Send HTTP POST request
+  int httpResponseCode = http.POST(url);
+  if (httpResponseCode == 200){
+    Serial.print("Message sent successfully");
+  }
+  else{
+    Serial.println("Error sending the message");
+    Serial.print("HTTP response code: ");
+    Serial.println(httpResponseCode);
+  }
+
+  // Free resources
+  http.end();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(pushPin, INPUT_PULLUP);
+
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  //Serial.println("button not pushed");
+  
+  var = digitalRead(pushPin);
+
+  if(var == LOW){
+    Serial.println("button pushed");
+    sendMessage("Emergency");
+  }
+}
